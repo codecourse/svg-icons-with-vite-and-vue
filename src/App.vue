5fae4d25898ecfcb740982d7be4e01223a36f741
Alex Garrett-Smith <alex@codecourse.com>
2023-06-29 11:15:35 +0100

    Episode 01

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-
+import Svg from './components/Svg.vue'
+import Bolt from './assets/svg/bolt.svg'
 </script>
 
 <template>
+    <!-- You can do this, but loads of imported components become cumbersome -->
+    <Bolt class="icon" />
 
+    <!-- This is better, one import with ability to pass the name of the svg -->
+    <Svg name="bolt" class="icon" />
 </template>
+
+<style>
+    .icon {
+        width: 50px;
+        height: 50px;
+    }
+</style>
